8eb785d3ef90a822fe7644a66296a339e7b7fe72
Nickop21 <ny8244@gmail.com>
2026-06-18 18:28:22 +0530

    conditional rendering of login and signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [iscurrStateSignIn, setIsCurStateSignIn] = useState(true);
+
+  function stateChange(islogin) {
+    if (islogin) {
+      setIsCurStateSignIn(false);
+    } else setIsCurStateSignIn(true);
+
+    console.log(islogin);
+  }
+
   return (
     <div className="relative h-150 w-full">
       <div className="absolute z-30 inset-0 p-6 bg-linear-to-b from-black/75  ">
@@ -12,14 +22,21 @@ const Login = () => {
         alt=""
         className="bg-red-600 object-cover h-full w-full"
       />
-      {/* <div className="absolute mx-auto "></div> */}
-      <form className="absolute inset-0 z-50 p-8 bg-black/50 h-2/3 w-1/3 m-auto text-amber-50 rounded">
-        <h2 className="text-2xl text-white font-bold">Sign In</h2>
+
+      <form className="absolute inset-0 z-50 p-10 bg-black/50 h-2/3 w-1/3 m-auto text-amber-50 rounded ">
+        <h2 className="text-2xl text-white font-bold">{iscurrStateSignIn?"Sign In" : "SIgnUp"}</h2>
         <div className="flex flex-col gap-6 mt-6">
+          {!iscurrStateSignIn && 
+           <input
+            type="text"
+            placeholder="Name"
+            className="p-2 bg-gray-800 w-full rounded border-none  outline-amber-700"
+          />
+          }
           <input
             type="text"
             placeholder="Email Address"
-            className="p-2 bg-gray-800 w-full rounded"
+            className="p-2 bg-gray-800 w-full rounded border-none  outline-amber-700"
           />
           <input
             type="text"
@@ -27,14 +44,34 @@ const Login = () => {
             className="p-2 bg-gray-800 w-full rounded"
           />
           <button className="bg-red-600 p-2 rounded cursor-pointer">
-            SignIn
+            {iscurrStateSignIn ? "SignIn" : "SignUP"}
           </button>
-          <div className="text-white font-bold ">
-            <span>New to Netflix? </span>
-            <span className="hover:underline cursor-pointer">Sign Up Now</span>
-          </div>
+          {iscurrStateSignIn ? (
+            <div className="text-white font-bold ">
+              <span>
+                {iscurrStateSignIn ? "New to Netflix" : "Already a user"} ?{" "}
+              </span>
+              <span
+                className="hover:underline cursor-pointer"
+                onClick={() => stateChange(true)}
+              >
+                Sign Up Now
+              </span>
+            </div>
+          ) : (
+            <div className="text-white font-bold ">
+              <span>Already registered? </span>
+              <span
+                className="hover:underline cursor-pointer"
+                onClick={() => stateChange(false)}
+              >
+                Sign In
+              </span>
+            </div>
+          )}
         </div>
       </form>
+
       <div className="absolute inset-0 bg-linear-to-t from-black via-black/40 to-black/30"></div>
     </div>
   );
