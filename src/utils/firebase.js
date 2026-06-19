be58d85828a07ea73ddce8a377b1f989b47ee339
Nickop21 <ny8244@gmail.com>
2026-06-19 19:22:23 +0530

    shift keys to env file and update gitignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,14 +7,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAhON9TeGYW20XVt6aZUmSwuZ6XikMyf3c",
-  authDomain: "netflixgpt-bb605.firebaseapp.com",
-  projectId: "netflixgpt-bb605",
-  storageBucket: "netflixgpt-bb605.firebasestorage.app",
-  messagingSenderId: "1001600521210",
-  appId: "1:1001600521210:web:d48c4ccec939302aae8a43",
-  measurementId: "G-DTL2HLHHB9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
